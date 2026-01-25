db8f4951e8af537f821de27991561cd9be0c60f3
hanshika.agarwal@algo-one.com <hanshikaag2021@gmail.com>
2026-01-25 12:34:39 +0530

    Intro to search agent with Tavily

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
-from typing import List
 
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
 from langchain_tavily import TavilySearch
 
+# from tavily import TavilyClient
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
+# tavily = TavilyClient()
 
-    url: str = Field(description="The URL of the source")
+# @tool
+# def search(query: str)->str:
+#     """
+#     Tool that searches over internet 
+#     Args:
+#        quey: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
-
-
-llm = ChatOpenAI(model="gpt-5")
+llm = ChatOllama(model="qwen2.5:3b")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
-
+agent = create_agent(model=llm,tools=tools)
 
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
-        }
-    )
+    print("Hello from lang-chain course!")
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job posting for an ai engineer using langchain in the bay area on linkedin and list their details")})
     print(result)
 
-
 if __name__ == "__main__":
-    main()
+    main() 
