d3c5b604ba4004f7e664726a670e0e5fd0ec020f
hanshika.agarwal@algo-one.com <hanshikaag2021@gmail.com>
2026-01-31 23:20:35 +0530

    structured_output

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.tools import tool
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
@@ -20,15 +20,16 @@ from prompt import REACT_PROMT_WITH_FORMAT_INSTUCTIONS
 from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOllama(model="qwen2.5:3b", temperatue=0)
+llm = ChatOllama(model="qwen2.5:3b", temperature=0)
+structured_llm =llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser=PydanticOutputParser(pydantic_object=AgentResponse)
+# output_parser=PydanticOutputParser(pydantic_object=AgentResponse)
 
 react_prompt_with_format_instructions = PromptTemplate(
     template= REACT_PROMT_WITH_FORMAT_INSTUCTIONS,
     input_variables=["input","agent_scratchpad","tool_names"]
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(
@@ -41,13 +42,13 @@ agent = create_react_agent(
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 extract_output= RunnableLambda(lambda x:x["output"])
-parse_output= RunnableLambda(lambda x:output_parser.parse(x))
+# parse_output= RunnableLambda(lambda x:output_parser.parse(x))
 # chain = agent_executor
 
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 def main():
-    result = agent_executor.invoke(
+    result = chain.invoke(
         input={
             "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
